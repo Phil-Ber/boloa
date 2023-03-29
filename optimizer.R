a05f183cc316a666517df2d30006bf54c0c045b0
Phil-Ber <Philippe.Berends@live.nl>
2023-03-29 08:43:22 +0000

    Changed tested chromwidth. Minpeakwidth is now a value between 1 and 5.

--- optimizer.R
+++ optimizer.R
@@ -11,14 +11,22 @@ set.seed(143241)
 register(bpstart(MulticoreParam(8)))
 ### Manually counted peaks with the corresponding file locations
 ## Lower peak limit
-lf <- list(c(243, 248, 253, 261, 336, 345, 470, 479, 567, 600, 672),
-           c(243,249,253,261,392,461,460,478,563,600,617,629,637,653,672,710),
-           c(243,248,253,262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
+# lf <- list(c(243, 248, 253, 261, 336, 345, 470, 479, 567, 600, 672),
+#            c(243,249,253,261,392,461,460,478,563,600,617,629,637,653,672,710),
+#            c(243,248,253,262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
+# )
+lf <- list(c(261, 336, 345, 470, 479, 567, 600, 672),
+           c(261,392,461,460,478,563,600,617,629,637,653,672,710),
+           c(262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
 )
 ## Upper peak limit
-uf <- list(c(248, 253, 258, 268, 340, 347, 474, 482, 571, 603, 676),
-           c(247,253,257,269,394,463,473,482,569,603,620,632,641,655,676,713),
-           c(248,253,258,268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
+# uf <- list(c(248, 253, 258, 268, 340, 347, 474, 482, 571, 603, 676),
+#            c(247,253,257,269,394,463,473,482,569,603,620,632,641,655,676,713),
+#            c(248,253,258,268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
+# )
+uf <- list(c(268, 340, 347, 474, 482, 571, 603, 676),
+           c(269,394,463,473,482,569,603,620,632,641,655,676,713),
+           c(268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
 )
 ## Array of paths to analyzed files.
 msfile <- c("/exports/nas/berends.p/boloa/massascans/062475b654c71a3df8103a49dcf4102fc7f503d1c886f8d6a91371ce.mzXML",
@@ -26,11 +34,12 @@ msfile <- c("/exports/nas/berends.p/boloa/massascans/062475b654c71a3df8103a49dcf
             "/exports/nas/berends.p/boloa/massascans/107c76e6b73bbb1c899aaac73696af6c24fbafa2670c152827a81cc8.mzXML"
 )
 ## How wide a peak can be
-peak_margin <<- 4
+peak_margin <<- 0
 
 pd <- data.frame(sample_name = c("Blanco2_1", "T0128_1150", 'QC4_6'), sample_group = c("Blanco2", "A", "QC4"), stringsAsFactors = FALSE)
 raw_data <- readMSData(files = msfile, pdata = new("NAnnotatedDataFrame", pd), mode = "onDisk")
-raw_data <<- filterRt(raw_data, c(min(rtime(raw_data)), 750))
+# raw_data <<- filterRt(raw_data, c(min(rtime(raw_data)), 750))
+raw_data <<- filterRt(raw_data, c(260, 750))
 
 detection <- function(detection_algo, params) {
   tryCatch(
@@ -66,7 +75,7 @@ detection <- function(detection_algo, params) {
           cor_det <- replace(length(corr_frame) / length(corr_frame), is.na(length(corr_frame) / length(corr_frame)), 0)
           n_corr <- n_corr + cor_det # cor_det is a binary value, either 0 or 1, 1 signifies a correct region is detected.
         }
-        n_corr <- n_corr - (-nrow(peaksf[[1]]) + n_corr)*2
+        n_corr <- n_corr - (nrow(peaksf[[1]]) + n_corr)**2
         fres <- c(fres, abs(1-length(lf[[file_index]])/(abs(n_corr)+length(lf[[file_index]]))-0.5))
       }
       return(as.data.frame(t(c(mean(fres), fres, params))))
@@ -83,7 +92,7 @@ options(warn=-1)
 edf <- detection(1, start_vals)
 for (i in 1:1000) {
   print(i)
-  minpeak <- runif(1,1.1,100)
+  minpeak <- runif(1,1.1,5)
   randvals <- c(
     runif(1, 1.1, 200), #ppm
     minpeak, #min_peakwidth
@@ -100,7 +109,7 @@ for (i in 1:1000) {
   edf <- rbind(edf, detection(1, randvals))
   #edf <- edf[edf[,1] != 1,]
 }
-write.csv(edf, "/exports/nas/berends.p/boloa/optimize_screen_5.csv", row.names=FALSE)
+write.csv(edf, "/exports/nas/berends.p/boloa/optimize_screen_7.csv", row.names=FALSE)
 
 tries <- 0
 prev_add <- data.frame(c(2,2,3))
@@ -150,6 +159,6 @@ while (mean(edf[,1]) >= 0.10 | tries < 100) {
   }
   edf <- rbind(edf, new_add)
   prev_add <- new_add
-  write.table(new_add, file = "/exports/nas/berends.p/boloa/optimize_screen_5.csv", sep = ",", append = TRUE, quote = FALSE,
+  write.table(new_add, file = "/exports/nas/berends.p/boloa/optimize_screen_7.csv", sep = ",", append = TRUE, quote = FALSE,
               col.names = FALSE, row.names = FALSE)
 }
