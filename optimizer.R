c0e425c01639f200999bebe75b2490e0173e8964
Phil-Ber <Philippe.Berends@live.nl>
2023-04-03 19:52:25 +0000

    Changed score formula for parameter combinations. Fully autonmated peak range selection. Now peaks are detected by looking ad square differences in tic. Super accurate. Method thougt up by mysels. Needs validation. Currently only used to set score values for optimization feature.

--- optimizer.R
+++ optimizer.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 suppressMessages(library(xcms))
 suppressMessages(library(dplyr))
 ### This script is used to optimize the parameters for peak detection. 
@@ -7,32 +8,58 @@ suppressMessages(library(dplyr))
 
 print("Running optimization... v2")
 
-set.seed(143241)
+set.seed(526262)
 register(bpstart(MulticoreParam(8)))
 ### Manually counted peaks with the corresponding file locations
 ## Lower peak limit
-# lf <- list(c(243, 248, 253, 261, 336, 345, 470, 479, 567, 600, 672),
-#            c(243,249,253,261,392,461,460,478,563,600,617,629,637,653,672,710),
-#            c(243,248,253,262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
+# # lf <- list(c(243, 248, 253, 261, 336, 345, 470, 479, 567, 600, 672),
+# #            c(243,249,253,261,392,461,460,478,563,600,617,629,637,653,672,710),
+# #            c(243,248,253,262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
+# # )
+# lf <- list(c(261, 336, 345, 470, 479, 567, 600, 672),
+#            c(261,392,461,460,478,563,600,617,629,637,653,672,710),
+#            c(262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
 # )
-lf <- list(c(261, 336, 345, 470, 479, 567, 600, 672),
-           c(261,392,461,460,478,563,600,617,629,637,653,672,710),
-           c(262,284,392,458,468,478,493,524,549,565,575,599,622,629,638,644,653,665,672,710)
-)
-## Upper peak limit
-# uf <- list(c(248, 253, 258, 268, 340, 347, 474, 482, 571, 603, 676),
-#            c(247,253,257,269,394,463,473,482,569,603,620,632,641,655,676,713),
-#            c(248,253,258,268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
+# ## Upper peak limit
+# # uf <- list(c(248, 253, 258, 268, 340, 347, 474, 482, 571, 603, 676),
+# #            c(247,253,257,269,394,463,473,482,569,603,620,632,641,655,676,713),
+# #            c(248,253,258,268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
+# # )
+# uf <- list(c(268, 340, 347, 474, 482, 571, 603, 676),
+#            c(269,394,463,473,482,569,603,620,632,641,655,676,713),
+#            c(268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
 # )
-uf <- list(c(268, 340, 347, 474, 482, 571, 603, 676),
-           c(269,394,463,473,482,569,603,620,632,641,655,676,713),
-           c(268,289,394,460,473,482,496,526,553,569,579,604,624,631,640,646,655,667,675,712)
-)
 ## Array of paths to analyzed files.
 msfile <- c("/exports/nas/berends.p/boloa/massascans/062475b654c71a3df8103a49dcf4102fc7f503d1c886f8d6a91371ce.mzXML",
             '/exports/nas/berends.p/boloa/massascans/0f4a154d2423caf8967d1d0275438c6c0d59c51e3d5fb5cec90b6cf4.mzXML',
             "/exports/nas/berends.p/boloa/massascans/107c76e6b73bbb1c899aaac73696af6c24fbafa2670c152827a81cc8.mzXML"
 )
+
+lf <- list()
+uf <- list()
+# A method of peak detection in order to retrieve peak ranges, not as accurate as CentWave or other methods.
+for (sampleN in 1:length(msfile)) {
+  raw_data <- readMSData(files = msfile[sampleN], pdata = new("NAnnotatedDataFrame"), mode = "onDisk")
+  raw_data <<- filterRt(raw_data, c(min(rtime(raw_data)), 750))
+  altered_tic <- tic(raw_data) - sqrt(tic(raw_data))
+  diffalt <- (altered_tic - tic(raw_data))/tic(raw_data)*100
+  trend <- supsmu(rtime(raw_data), diffalt, bass = -100)
+  trend$y <- trend$y - mean(diffalt) / 5
+  newvals <- tic(raw_data)
+  newvals[diffalt < trend$y] <- NA
+  minranges <- c()
+  maxranges <- c()
+  for (i in 2:length(newvals)) {
+    if (is.na(newvals[i]) != TRUE & is.na(newvals[i - 1])) {
+      minranges <- c(minranges, rtime(raw_data)[i])
+    }
+    if (is.na(newvals[i]) == TRUE & is.na(newvals[i - 1]) == FALSE) {
+      maxranges <- c(maxranges, rtime(raw_data)[i])
+    }
+  }
+  lf[[sampleN]] <- minranges
+  uf[[sampleN]] <- maxranges
+}
 ## How wide a peak can be
 peak_margin <<- 0
 
@@ -48,6 +75,7 @@ detection <- function(detection_algo, params) {
         xset <<- suppressMessages(findChromPeaks(raw_data, param = CentWaveParam(
           ppm = params[1],
           peakwidth = c(params[2], params[3]),
+          # peakwidth = c(1, params[2]),
           snthresh = params[4],
           prefilter = c(round(params[5]), params[6]),
           mzCenterFun = c("wMean", "mean", "apex", "wMeanApex3", "meanApex3")[round(params[7])],
@@ -75,8 +103,9 @@ detection <- function(detection_algo, params) {
           cor_det <- replace(length(corr_frame) / length(corr_frame), is.na(length(corr_frame) / length(corr_frame)), 0)
           n_corr <- n_corr + cor_det # cor_det is a binary value, either 0 or 1, 1 signifies a correct region is detected.
         }
-        n_corr <- n_corr - (nrow(peaksf[[1]]) + n_corr)**2
-        fres <- c(fres, abs(1-length(lf[[file_index]])/(abs(n_corr)+length(lf[[file_index]]))-0.5))
+        n_corr <- n_corr - (nrow(peaksf[[1]]) + n_corr)
+        fres <- c(fres, pmax(0, 1 - abs(abs(n_corr) - length(lf[[file_index]])) / ((abs(n_corr) - length(lf[[file_index]]))^2)/2))
+        
       }
       return(as.data.frame(t(c(mean(fres), fres, params))))
     },
@@ -86,40 +115,49 @@ detection <- function(detection_algo, params) {
   )
 }
 
-
-start_vals <- c(93, 5, 25, 100, 1, 300, 1, 100, 1, 1000, 0)
-options(warn=-1)
-edf <- detection(1, start_vals)
-for (i in 1:1000) {
-  print(i)
-  minpeak <- runif(1,1.1,5)
-  randvals <- c(
-    runif(1, 1.1, 200), #ppm
-    minpeak, #min_peakwidth
-    runif(1, minpeak, 101+minpeak), #min+val = max_peakwith
-    runif(1, 1.1, 100), #snthresh
-    sample(c(1,2,3), 1), #prefilter
-    runif(1, 1.1, 400), #val_of_prefilter
-    sample(c(1,2,3,4,5), 1), #mzCenterFun
-    runif(1, -1.1, 1), #mzdiff
-    sample(c(0,1), 1), #fitgauss
-    runif(1, 0.1, 10000), #noise
-    sample(c(0,1), 1) #verbosecolumns
-  )
-  edf <- rbind(edf, detection(1, randvals))
-  #edf <- edf[edf[,1] != 1,]
-}
-write.csv(edf, "/exports/nas/berends.p/boloa/optimize_screen_7.csv", row.names=FALSE)
-
+# start_vals <- c(93, 5, 25, 100, 1, 300, 1, 100, 1, 1000, 0)
+# # start_vals <- c(93, 25, 100, 1, 300, 1, 100, 1, 1000, 0)
+# options(warn=-1)
+# edf <- detection(1, start_vals)
+# for (i in 1:1000) {
+#   print(i)
+#   print(Sys.time())
+#   minpeak <- runif(1,1.1,10)
+#   randvals <- c(
+#     runif(1, 1.1, 200), #ppm
+#     minpeak, #min_peakwidth
+#     runif(1, minpeak, 101+minpeak), #min+val = max_peakwith
+#     runif(1, 1.1, 100), #snthresh
+#     sample(c(1,2,3), 1), #prefilter
+#     runif(1, 1.1, 400), #val_of_prefilter
+#     sample(c(1,2,3,4,5), 1), #mzCenterFun
+#     runif(1, -1.1, 1), #mzdiff
+#     sample(c(0,1), 1), #fitgauss
+#     runif(1, 0.1, 10000), #noise
+#     sample(c(0,1), 1) #verbosecolumns
+#   )
+#   edf <- rbind(edf, detection(1, randvals))
+#   edf <- edf[edf[,1] != 1,]
+# }
+# write.csv(edf, "/exports/nas/berends.p/boloa/randomgen_param_for_optimization.csv", row.names=FALSE)
+edf <- read.csv("/exports/nas/berends.p/boloa/randomgen_param_for_optimization.csv", header
+                =TRUE, stringsAsFactors=FALSE)
+edf <- na.omit(edf)
 tries <- 0
-prev_add <- data.frame(c(2,2,3))
+prev_add <- tail(edf, n = 1)
+current_best <- c(t(edf[order(edf[,1]),][1,]))
 while (mean(edf[,1]) >= 0.10 | tries < 100) {
+  edf <- na.omit(edf)
+  previous_params <- c(t(prev_add))
+  if (previous_params[1] < current_best[1]) {
+    current_best <- previous_params
+  }
   edf <- tail(edf, n = 1000) #For testing
-  end_vals <- c(t(colMeans(edf[,5:length(edf)])))
-  end_vals <- (end_vals + c(t(edf[order(edf[,1]),][1,5:15]))) / 2
-  #message("N TRIES:")
-  #message(tries)
-  if (tries %% 5 == 0) {
+  mean_over_edf <- c(t(colMeans(edf[,5:length(edf)])))
+  end_vals <- (mean_over_edf + current_best[5:15] + previous_params[5:15]) / 3
+  # message("N TRIES:")
+  # message(tries)
+  if (tries %% 2 == 0) {
     npars <- c()
     for (i in 1:length(end_vals)) {
       GSfactor <- -cor(edf[,i+4], edf[,1])
@@ -127,22 +165,22 @@ while (mean(edf[,1]) >= 0.10 | tries < 100) {
     }
     
   }
-  else if (tries %% 12 == 0) {
+  else if (tries %% 5 == 0) {
     npars <- c()
     for (i in 1:length(end_vals)) {
       GSfactor <- -cor(edf[,i+4], edf[,1])
-      npars <- c(npars, end_vals[i] * (GSfactor**3 + 1))
+      npars <- c(npars, end_vals[i] * ((GSfactor)**3 + 1))
     }
     edf <- edf[edf[,1] != 1,]
   }
-  else if (tries %% 49 == 0) {
-    npars <- c()
-    for (i in 1:length(end_vals)) {
-      GSfactor <- -cor(edf[,i+4], edf[,1])
-      npars <- c(npars, end_vals[i] * (GSfactor**4 + 1))
-    }
-    edf <- edf[edf[,1] != 1,]
-  }
+  # else if (tries %% 11 == 0) {
+  #   npars <- c()
+  #   for (i in 1:length(end_vals)) {
+  #     GSfactor <- -cor(edf[,i+4], edf[,1])
+  #     npars <- c(npars, end_vals[i] * ((GSfactor^-(1/2)) + 1))
+  #   }
+  #   edf <- edf[edf[,1] != 1,]
+  # }
   else {
     npars <- c()
     for (i in 1:length(end_vals)) {
@@ -155,10 +193,10 @@ while (mean(edf[,1]) >= 0.10 | tries < 100) {
   if (prev_add[,1][1] == new_add[,1]) {
     tries <- tries + 1
   } else {
-    tries <- 0 
+    #tries <- 0 
   }
   edf <- rbind(edf, new_add)
   prev_add <- new_add
-  write.table(new_add, file = "/exports/nas/berends.p/boloa/optimize_screen_7.csv", sep = ",", append = TRUE, quote = FALSE,
+  write.table(new_add, file = "/exports/nas/berends.p/boloa/optimize_screen_10.csv", sep = ",", append = TRUE, quote = FALSE,
               col.names = FALSE, row.names = FALSE)
 }
